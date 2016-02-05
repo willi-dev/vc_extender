321909285954f09cffe418810b37f68dbd3d75e8
willi-dev <willi.ilmukomputer@gmail.com>
2016-02-05 09:41:09 +0700

    df_vc_extender
    add comment; construct function

---
+++ df_vc_extender.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Dahz Framework
+ * Class: vc_extender
+ * Description: extender for visual composer wordpress plugin
+ */
+
+class vc_extender {
+
+	/**
+	 * __construct()
+	 */
+	function __construct() {
+		
+	}
+
+}
+
+/* location: [theme directory]/core/vc_extender/df_vc_extender.php
