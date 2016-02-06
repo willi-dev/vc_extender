dfdcfbdcbb02fba4d69d6ed55361b8953f1fe738
willi-dev <willi.ilmukomputer@gmail.com>
2016-02-06 13:52:06 +0700

    modified
    modified comment; add function df_posts_grid for testing load posts grid

--- df_vc_extender.php
+++ df_vc_extender.php
@@ -1,27 +1,26 @@
 <?php
 /**
  * Dahz Framework
+ * df_vc_extender.php
  * Class: vc_extender
  * Description: extender for visual composer wordpress plugin
  */
 
+define("VC_EXTENDER_CATEGORY", "by Dahz Themes");
+
+
 class vc_extender {
+
+	// $array_of_type = array();
+	var $array_of_function = array();
 
 	/**
 	 * __construct()
 	 */
 	function __construct() {
-		add_action( 'vc_before_init', array( $this, 'df_block' ) );
-
-		add_shortcode( 'df_block_1', array($this, 'render_df_block_1') );
-
-		add_action( 'vc_before_init', array( $this, 'df_add_image') );
-
-		add_shortcode('df_block_img', array($this, 'render_df_block_img') );
-
-		if( function_exists( 'vc_manager' ) ) {
-			vc_manager()->disableUpdater( true );
-		}
+		add_action( 'init', array( $this, 'df_block' ) );
+		add_action( 'init', array( $this, 'df_add_image') );
+		add_action( 'init', array( $this, 'df_posts_grid') );
 	}
 
 	function df_block(){
@@ -29,7 +28,7 @@ class vc_extender {
 			array(
 			    "name" => __("Block Grid 1", "vc_extender"), // add a name
             	"description" => __("Bar tag description text", 'vc_extender'),
-            	"category" => "Block",
+            	"category" => VC_EXTENDER_CATEGORY,
 			    "base" => "df_block_1", // bind with our shortcode
 			    "content_element" => true, // set this parameter when element will has a content
 			    "is_container" => true, // set this param when you need to add a content element in this element
@@ -74,7 +73,7 @@ class vc_extender {
 			array(
 				"name" => __("Block Add Image", "vc_extender"),
 				"description" => __("Bar tag description text", 'vc_extender'),
-            	"category" => "Block",
+            	"category" => VC_EXTENDER_CATEGORY,
 			    "base" => "df_block_img", 
 			    "content_element" => true, 
 			    "is_container" => true, 
@@ -85,39 +84,57 @@ class vc_extender {
 			    			"class" => "",
 			    			"heading" => __("Text", "vc_extender"),
 			    			"description" => __("description for attach image"), 
-			    			"param_name" => "df_block_img"
+			    			"param_name" => "image"
 			    		)
 			    	)
 			)
 		);
 	}
 
-
-	public function render_df_block_1( $atts, $content = null ){
-		extract( shortcode_atts(array(
-				'foo' => 'something in the way',
-				'color' => '#FF0000'
-			), $atts) 
+	function df_posts_grid() {
+		vc_map(
+			array(
+				"name" => __("Posts Grid", "vc_extender"),
+				"description" => __("Display your posts in grid view", "vc_extender"),
+				"category" => VC_EXTENDER_CATEGORY,
+				"base" => "df_posts_grid",
+				"content_element" => true,
+				"is_container" => true,
+				"params" => array(
+						array(
+							"type" => "dropdown",
+							"heading" => "Style", 
+							"param_name" => "style",
+							"admin_label" => "style",
+							"value" => array(
+								"style 1" => "style 1",
+								"style 2" => "style 2",
+								"style 3" => "style 3"
+							),
+							"description" => "Changes the layouts style of the grid"
+						),array(
+							"type" => "dropdown",
+							"heading" => "Post Source",
+							"param_name" => "source",
+							"value" => array(
+								"Most Recent" => "most-recent",
+								"By Category" => "by-category",
+								"By Post ID" => "by-id",
+								"By Tag" => "by-tag",
+								"By Author" => "by-author"
+							),
+							"std" => "most-recent",
+							"admin_label" => true,
+							"description" => "Select the source of the posts"
+						)
+				)
+			)
 		);
-		$content = wpb_js_remove_wpautop($content, true); 
-
-		$output = "<div style='background-color: {$color};' data-foo='{$foo}'>{$content}</div>";
-		return $output;
 	}
 
-	public function render_df_block_img( $atts, $content = null ) {
-		extract( shortcode_atts(array(
-				'df_block_img' => ''
-			), $atts)
-		);
-		$content = wpb_js_remove_wpautop($content, true);
-		$output = "<div style=''><img src='{$df_block_img}'></div>";
-		return $output;
-	}
-
+	
 }
-
-// This function provides a functionality of adding content elements into element
-// class WPBakeryShortCode_SC_Slide extends WPBakeryShortCodesContainer {}
-
+// shortcode vc_extender
+require_once('df_vc_extender-shortcode.php');
+new vc_extender_shortcode();
 /* location: [theme directory]/core/vc_extender/df_vc_extender.php */
