44be397850b1a812f8eeb96766423d2ac1ba98bc
willi-dev <willi.ilmukomputer@gmail.com>
2016-02-11 15:22:17 +0700

    update
    add shortcode function

--- df_vc_extender-shortcode.php
+++ df_vc_extender-shortcode.php
@@ -8,21 +8,26 @@
 
 class df_vc_extender_shortcode {
 
-	// private $array_map_shortcode = array();
 	/**
 	 * __construct()
 	 */
-	function __construct() {
+	function __construct( $array_map ) {
 		if(!function_exists('vc_map_get_attributes')){
 			return;
 		}else{
-			// add_shortcode( 'df_block_1', array($this, 'render_df_block_1') );
-			// add_shortcode( 'df_block_img', array($this, 'render_df_block_img') );
+			$array_map_block = $array_map;
 
-			add_shortcode('df_posts_block_1', array($this, 'df_posts_block_1'));
+			foreach ( $array_map_block as $amb) {
+				extract($amb);
+				add_shortcode( $base, array( $this , $base ) );
+			}
 		}
 	}
 
+	/**
+	 * df_posts_block_1
+	 * render shortcode output to frontend for posts block 1
+	 */
 	function df_posts_block_1( $atts, $content = null ){
 		$atts = vc_map_get_attributes( 'df_posts_block_1', $atts );
 		extract( $atts );
@@ -90,59 +95,426 @@ class df_vc_extender_shortcode {
 		$posts = query_posts( $args );
 
 		ob_start();
+		
+		$title_text_color = ( isset($title_text_color) ) ? $title_text_color : '';
 
-		if( have_posts() ) { ?>
-			
-			<div class="row posts">
-				
-				<?php 
+		?>
+		
+		<div class="df-vc-title-block" style="color:<?php echo $title_text_color;?> ;">
+			<?php echo $title;?>
+		</div>
+		<?php
+
+		if( have_posts() ) { 
+		?>
+		
+		<div class="row posts">
+		<?php
 				while( have_posts() ) : the_post(); 
-
-				// content here
-
-				endwhile;
 				?>
-
-			</div>
-
+						<div class="col-md-3">
+						<?php if (has_post_thumbnail() ) {
+						?>
+						
+							<figure class="df-post-thumbnail">
+								<a href="<?php the_permalink();?>" title="<?php the_title();?>" rel="bookmark">
+									<?php the_post_thumbnail('post-thumbnail', array('itemprop' => 'image', 'class' => 'img-responsive')); ?>
+								</a>
+							</figure>
+						<?php
+						}?>	
+							<header class="post-title entry-header">
+								<?php the_title('<h5 class="entry-title" itemprop="name headline"><a href="'.get_permalink().'" title="'.the_title_attribute("echo=0").'">', '</a></h5>'); ?>
+							</header>
+							<!-- <div class="post-content entry-content small">
+								<?php //echo the_content(); ?>
+							</div> -->
+						</div>
+				
+				<?php
+				endwhile;?>
+		</div>	
 		<?php
 		}
+
 		$out = ob_get_contents();
+
 		if (ob_get_contents()) ob_end_clean();
 		wp_reset_query();
 		wp_reset_postdata();
 
 		return $out;
+
 	}
 
-	// public function render_df_block_1( $atts, $content = null ){
-	// 	extract( shortcode_atts(array(
-	// 			'foo' => 'something in the way',
-	// 			'color' => '#FF0000'
-	// 		), $atts) 
-	// 	);
-	// 	$content = wpb_js_remove_wpautop($content, true); 
+	/**
+	 * df_posts_block_2
+	 * render shortcode output to frontend for posts block 2
+	 */
+	function df_posts_block_2( $atts, $content = null ){
+		$atts = vc_map_get_attributes( 'df_posts_block_2', $atts );
+		extract( $atts );
 
-	// 	$output = "<div style='background-color: {$color};' data-foo='{$foo}'>{$content}</div>";
-	// 	return $output;
-	// }
+		$args = array(
+			'post_type' => 'post',
+			'post_status' => 'publish',
+			'ignore_sticky_posts' => 1
+		);
+		// if offset not null
+		if( $offset ){
+			$args = wp_parse_args(
+				array(
+					'offset' => $offset
+				)
+			, $args);
+		}
 
-	// public function render_df_block_img( $atts, $content = null ) {
-	// 	// pindahkan ke __construct() ?
-	// 	// if(!function_exists('vc_map_get_attributes')){
-	// 	// 	return;
-	// 	// } 
+		if( $source == 'most-recent' ){ // if source most-recent
+			$args = wp_parse_args(
+				array(
+					'posts_per_page' => $limit_post_number
+				)
+			, $args);
+		}else if( $source == 'by-category' ){ // if source by-category
+			if(!empty($categories)){
+				$cats = explode(',',$categories);
+				$args = wp_parse_args(
+					array(
+						'posts_per_page' => $limit_post_number, 
+						'category__in' => $cats
+					)
+				, $args);
+			}
+		}else if( $source == 'by-post-id' ){ // if source by-post-id
+			$posts = explode(',', $post_ids);
 
-	// 	$atts = vc_map_get_attributes( 'df_block_img', $atts );
-	// 	extract( $atts );
-	// 		$img_id = preg_replace('/[^\d]/', '', $image);
-	// 	$img_size = "full";
-	// 	$atts = array('class' => 'img-responsive center-block');
+			$args = wp_parse_args(
+				array(
+					'post__in' => $posts,
+					'posts_per_page' => $limit_post_number,
+					'orderby' => 'post__in'
+				)
+			, $args);
+		}else if( $source == 'by-tag' ){ // if source by-tag
+			$tags = explode(',', $tag_slugs);
 
-	// 	$img = wp_get_attachment_image($img_id, $img_size, false, $atts);
-	// 	// print_r($atts);
-	// 	$output = '<div class="row"><div class="col-md-6">'.$img.'</div></div>';
-	// 	return $output;
-	// }
+			$args = wp_parse_args(
+				array(
+					'posts_per_page' => $limit_post_number,
+					'tag_slug__in' => $tags
+				)
+			, $args);
+		}else if( $source == 'by-author' ){ // if source by-author
+			$authors = explode(',', $author_ids);
+
+			$args = wp_parse_args(
+				array(
+					'posts_per_page' => $limit_post_number,
+					'author__in' => $authors
+				)
+			, $args);
+		}
+
+		$posts = query_posts( $args );
+
+		ob_start();
+		
+		$title_text_color = ( isset($title_text_color) ) ? $title_text_color : '';
+
+		?>
+		
+		<div class="df-vc-title-block" style="color:<?php echo $title_text_color;?> ;">
+			<?php echo $title;?>
+		</div>
+		<?php
+
+		if( have_posts() ) { 
+		?>
+		
+		<div class="row posts">
+		<?php
+				while( have_posts() ) : the_post(); 
+				?>
+						<div class="col-md-3">
+						<?php if (has_post_thumbnail() ) {
+						?>
+						
+							<figure class="df-post-thumbnail">
+								<a href="<?php the_permalink();?>" title="<?php the_title();?>" rel="bookmark">
+									<?php the_post_thumbnail('post-thumbnail', array('itemprop' => 'image', 'class' => 'img-responsive')); ?>
+								</a>
+							</figure>
+						<?php
+						}?>	
+							<header class="post-title entry-header">
+								<?php the_title('<h5 class="entry-title" itemprop="name headline"><a href="'.get_permalink().'" title="'.the_title_attribute("echo=0").'">', '</a></h5>'); ?>
+							</header>
+							<!-- <div class="post-content entry-content small">
+								<?php //echo the_content(); ?>
+							</div> -->
+						</div>
+				
+				<?php
+				endwhile;?>
+		</div>	
+		<?php
+		}
+
+		$out = ob_get_contents();
+
+		if (ob_get_contents()) ob_end_clean();
+		wp_reset_query();
+		wp_reset_postdata();
+
+		return $out;
+
+	}
+
+	/**
+	 * df_posts_block_3
+	 * render shortcode output to frontend for posts block 3
+	 */
+	function df_posts_block_3( $atts, $content = null ){
+		$atts = vc_map_get_attributes( 'df_posts_block_3', $atts );
+		extract( $atts );
+
+		$args = array(
+			'post_type' => 'post',
+			'post_status' => 'publish',
+			'ignore_sticky_posts' => 1
+		);
+		// if offset not null
+		if( $offset ){
+			$args = wp_parse_args(
+				array(
+					'offset' => $offset
+				)
+			, $args);
+		}
+
+		if( $source == 'most-recent' ){ // if source most-recent
+			$args = wp_parse_args(
+				array(
+					'posts_per_page' => $limit_post_number
+				)
+			, $args);
+		}else if( $source == 'by-category' ){ // if source by-category
+			if(!empty($categories)){
+				$cats = explode(',',$categories);
+				$args = wp_parse_args(
+					array(
+						'posts_per_page' => $limit_post_number, 
+						'category__in' => $cats
+					)
+				, $args);
+			}
+		}else if( $source == 'by-post-id' ){ // if source by-post-id
+			$posts = explode(',', $post_ids);
+
+			$args = wp_parse_args(
+				array(
+					'post__in' => $posts,
+					'posts_per_page' => $limit_post_number,
+					'orderby' => 'post__in'
+				)
+			, $args);
+		}else if( $source == 'by-tag' ){ // if source by-tag
+			$tags = explode(',', $tag_slugs);
+
+			$args = wp_parse_args(
+				array(
+					'posts_per_page' => $limit_post_number,
+					'tag_slug__in' => $tags
+				)
+			, $args);
+		}else if( $source == 'by-author' ){ // if source by-author
+			$authors = explode(',', $author_ids);
+
+			$args = wp_parse_args(
+				array(
+					'posts_per_page' => $limit_post_number,
+					'author__in' => $authors
+				)
+			, $args);
+		}
+
+		$posts = query_posts( $args );
+
+		ob_start();
+		
+		$title_text_color = ( isset($title_text_color) ) ? $title_text_color : '';
+
+		?>
+		
+		<div class="df-vc-title-block" style="color:<?php echo $title_text_color;?> ;">
+			<?php echo $title;?>
+		</div>
+		<?php
+
+		if( have_posts() ) { 
+		?>
+		
+		<div class="row posts">
+		<?php
+				while( have_posts() ) : the_post(); 
+				?>
+						<div class="col-md-3">
+						<?php if (has_post_thumbnail() ) {
+						?>
+						
+							<figure class="df-post-thumbnail">
+								<a href="<?php the_permalink();?>" title="<?php the_title();?>" rel="bookmark">
+									<?php the_post_thumbnail('post-thumbnail', array('itemprop' => 'image', 'class' => 'img-responsive')); ?>
+								</a>
+							</figure>
+						<?php
+						}?>	
+							<header class="post-title entry-header">
+								<?php the_title('<h5 class="entry-title" itemprop="name headline"><a href="'.get_permalink().'" title="'.the_title_attribute("echo=0").'">', '</a></h5>'); ?>
+							</header>
+							<!-- <div class="post-content entry-content small">
+								<?php //echo the_content(); ?>
+							</div> -->
+						</div>
+				
+				<?php
+				endwhile;?>
+		</div>	
+		<?php
+		}
+
+		$out = ob_get_contents();
+
+		if (ob_get_contents()) ob_end_clean();
+		wp_reset_query();
+		wp_reset_postdata();
+
+		return $out;
+
+	}
+
+	/**
+	 * df_posts_block_4
+	 * render shortcode output to frontend for posts block 4
+	 */
+	function df_posts_block_4( $atts, $content = null ){
+		$atts = vc_map_get_attributes( 'df_posts_block_4', $atts );
+		extract( $atts );
+
+		$args = array(
+			'post_type' => 'post',
+			'post_status' => 'publish',
+			'ignore_sticky_posts' => 1
+		);
+		// if offset not null
+		if( $offset ){
+			$args = wp_parse_args(
+				array(
+					'offset' => $offset
+				)
+			, $args);
+		}
+
+		if( $source == 'most-recent' ){ // if source most-recent
+			$args = wp_parse_args(
+				array(
+					'posts_per_page' => $limit_post_number
+				)
+			, $args);
+		}else if( $source == 'by-category' ){ // if source by-category
+			if(!empty($categories)){
+				$cats = explode(',',$categories);
+				$args = wp_parse_args(
+					array(
+						'posts_per_page' => $limit_post_number, 
+						'category__in' => $cats
+					)
+				, $args);
+			}
+		}else if( $source == 'by-post-id' ){ // if source by-post-id
+			$posts = explode(',', $post_ids);
+
+			$args = wp_parse_args(
+				array(
+					'post__in' => $posts,
+					'posts_per_page' => $limit_post_number,
+					'orderby' => 'post__in'
+				)
+			, $args);
+		}else if( $source == 'by-tag' ){ // if source by-tag
+			$tags = explode(',', $tag_slugs);
+
+			$args = wp_parse_args(
+				array(
+					'posts_per_page' => $limit_post_number,
+					'tag_slug__in' => $tags
+				)
+			, $args);
+		}else if( $source == 'by-author' ){ // if source by-author
+			$authors = explode(',', $author_ids);
+
+			$args = wp_parse_args(
+				array(
+					'posts_per_page' => $limit_post_number,
+					'author__in' => $authors
+				)
+			, $args);
+		}
+
+		$posts = query_posts( $args );
+
+		ob_start();
+		
+		$title_text_color = ( isset($title_text_color) ) ? $title_text_color : '';
+
+		?>
+		
+		<div class="df-vc-title-block" style="color:<?php echo $title_text_color;?> ;">
+			<?php echo $title;?>
+		</div>
+		<?php
+
+		if( have_posts() ) { 
+		?>
+		
+		<div class="row posts">
+		<?php
+				while( have_posts() ) : the_post(); 
+				?>
+						<div class="col-md-3">
+						<?php if (has_post_thumbnail() ) {
+						?>
+						
+							<figure class="df-post-thumbnail">
+								<a href="<?php the_permalink();?>" title="<?php the_title();?>" rel="bookmark">
+									<?php the_post_thumbnail('post-thumbnail', array('itemprop' => 'image', 'class' => 'img-responsive')); ?>
+								</a>
+							</figure>
+						<?php
+						}?>	
+							<header class="post-title entry-header">
+								<?php the_title('<h5 class="entry-title" itemprop="name headline"><a href="'.get_permalink().'" title="'.the_title_attribute("echo=0").'">', '</a></h5>'); ?>
+							</header>
+							<!-- <div class="post-content entry-content small">
+								<?php //echo the_content(); ?>
+							</div> -->
+						</div>
+				
+				<?php
+				endwhile;?>
+		</div>	
+		<?php
+		}
+
+		$out = ob_get_contents();
+
+		if (ob_get_contents()) ob_end_clean();
+		wp_reset_query();
+		wp_reset_postdata();
+
+		return $out;
+
+	}
 
 }
+/* file location: [theme directory]/core/vc_extender/df_vc_extender-shortcode.php */
